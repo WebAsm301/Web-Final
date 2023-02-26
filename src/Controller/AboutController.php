c8edffcdc3e88e0cb3ea5b0822bc0112660484a5
TrThinh <TrongThinh2602@gmail.com>
2023-02-27 02:35:54 +0700

    commit night

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -7,14 +7,15 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AboutController extends AbstractController
-{
-    /**
-     * @Route("/about", name="app_about")
-     */
-    public function index(): Response
     {
-        return $this->render('about/index.html.twig', [
-            'controller_name' => 'AboutController',
-        ]);
+        /**
+         * @Route("/about", name="app_about")
+         */
+        public function index(): Response
+        {
+            return $this->render('about/index.html.twig', [
+                'controller_name' => 'AboutController',
+            ]);
+        }
+    
     }
-}
