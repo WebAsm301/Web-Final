4b077fde846c76528a081bbb70c59ea0448cdb11
TrThinh <TrongThinh2602@gmail.com>
2023-02-26 15:24:50 +0700

    first commit

---
+++ src/Entity/Orders.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrdersRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=OrdersRepository::class)
+ */
+class Orders
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $Date;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $Delivery;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $Status;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->Date;
+    }
+
+    public function setDate(\DateTimeInterface $Date): self
+    {
+        $this->Date = $Date;
+
+        return $this;
+    }
+
+    public function getDelivery(): ?\DateTimeInterface
+    {
+        return $this->Delivery;
+    }
+
+    public function setDelivery(\DateTimeInterface $Delivery): self
+    {
+        $this->Delivery = $Delivery;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->Status;
+    }
+
+    public function setStatus(?string $Status): self
+    {
+        $this->Status = $Status;
+
+        return $this;
+    }
+}
