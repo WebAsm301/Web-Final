9b6969809ae139f743737c1e010cac4b1ecd54d0
TrThinh <TrongThinh2602@gmail.com>
2023-02-26 17:27:40 +0700

    Initial commit

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,19 +2,114 @@
 
 namespace App\Controller;
 
+use App\Entity\Products;
+use App\Form\ProductsType;
+use App\Repository\ProductsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Form\AddToCartType;
+use App\Manager\CartManager;
+/**
+ * @Route("/products")
+ */
 class ProductsController extends AbstractController
 {
     /**
-     * @Route("/products", name="app_products")
+     * @Route("/", name="app_products_index", methods={"GET", "POST"})
      */
-    public function index(): Response
+    public function index(ProductsRepository $productsRepository): Response
     {
         return $this->render('products/index.html.twig', [
-            'controller_name' => 'ProductsController',
+            'products' => $productsRepository->findAll(),
         ]);
+    }
+    /**
+     * @Route("/new", name="app_products_new", methods={"GET", "POST"})
+     */
+    #[Route('/new', name: 'app_products_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, ProductsRepository $productsRepository): Response
+    {
+        $product = new Products();
+        $form = $this->createForm(ProductsType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $productsRepository->add($product);
+            return $this->redirectToRoute('app_products_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('products/new.html.twig', [
+            'product' => $product,
+            'form' => $form,
+        ]);
+    }
+    /**
+     * @Route("/product/{id}/detail", name="product.detail")
+     */
+    public function detail(Products $product, Request $request, CartManager $cartManager): Response
+    {
+        $form = $this->createForm(AddToCartType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $item = $form->getData();
+            $item->setproduct($product);
+
+            $cart = $cartManager->getCurrentCart();
+            $cart
+                ->addItem($item)
+                ->setDate(new \DateTime());
+
+            $cartManager->save($cart);
+
+            return $this->redirectToRoute('product.detail', ['id' => $product->getId()]);
+        }
+        return $this->render('products/detail.html.twig', [
+            'product' => $product,
+            'form' => $form->createView()
+        ]);
+    }
+
+
+    /**
+     * @Route("/{id}", name="app_products_show", methods={"GET"})
+     */
+    public function show(Products $product): Response
+    {
+        return $this->render('products/show.html.twig', [
+            'product' => $product,
+        ]);
+    }
+    
+    /**
+     * @Route("/{id}/edit", name="app_products_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Products $product, ProductsRepository $productsRepository): Response
+    {
+        $form = $this->createForm(ProductsType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $productsRepository->add($product);
+            return $this->redirectToRoute('app_products_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('products/edit.html.twig', [
+            'product' => $product,
+            'form' => $form,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="app_products_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Products $product, ProductsRepository $productsRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$product->getId(), $request->request->get('_token'))) {
+            $productsRepository->remove($product);
+        }
+
+        return $this->redirectToRoute('app_products_index', [], Response::HTTP_SEE_OTHER);
     }
 }
