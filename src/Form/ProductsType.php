5250668bd316ac0a30654aa0a5ce149ce34d305c
TBot <101082151+TrThinh@users.noreply.github.com>
2023-03-02 02:45:50 +0700

    Update ProductsType.php

--- src/Form/ProductsType.php
+++ src/Form/ProductsType.php
@@ -3,21 +3,24 @@
 namespace App\Form;
 
 use App\Entity\Products;
-use PHPUnit\TextUI\CliArguments\Builder;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use PHPUnit\TextUI\CliArguments\Builder;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+
 
 class ProductsType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('Name')
-            ->add('Description')
+            ->add('Name', TextType::class)
+            ->add('Description', TextType::class)
             ->add('Price')
-            ->add('Quantity')
-            ->add('Status')
+            ->add('Quantity', IntegerType::class, ['data' => 1])
+            ->add('Status', TextType::class)
         ;
     }
 
