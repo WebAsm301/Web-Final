550a8607f598c1e6accd0df6261ae7ac99a1cbd0
minhhieu1603 <125235732+minhhieu1603@users.noreply.github.com>
2023-03-01 20:04:11 +0700

    Update OrdersController.php

--- src/Controller/OrdersController.php
+++ src/Controller/OrdersController.php
@@ -6,19 +6,18 @@ use App\Entity\Orders;
 use App\Form\OrdersType;
 use App\Repository\OrdersRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Form;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
-
+use App\Form\AbstractType;
 /**
  * @Route("/orders")
  */
 class OrdersController extends AbstractController
 {
-    
     /**
-     * @Route("/", name="app_orders_index", methods={"GET"})
+     * @Route("/", name="app_orders_index", methods={"GET", "POST"})
      */
     public function index(OrdersRepository $ordersRepository): Response
     {
@@ -26,18 +25,23 @@ class OrdersController extends AbstractController
             'orders' => $ordersRepository->findAll(),
         ]);
     }
-
     /**
+     * Create a new order.
+     * 
      * @Route("/new", name="app_orders_new", methods={"GET", "POST"})
      */
     public function new(Request $request, OrdersRepository $ordersRepository): Response
     {
         $order = new Orders();
-        $form = $this->createForm(OrdersType::class, $order);
+        $form = $this->createForm('App\Form\OrdersType', $order);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $ordersRepository->add($order);
+            $ordersRepository = $this->getDoctrine()->getManager();
+            $ordersRepository->persist($order);
+            $ordersRepository->flush();
+            
             return $this->redirectToRoute('app_orders_index', [], Response::HTTP_SEE_OTHER);
         }
 
@@ -47,47 +51,73 @@ class OrdersController extends AbstractController
         ]);
     }
 
-    
     /**
-     * @Route("/{id}", name="app_orders_show", methods={"GET"})
-     */
-    public function show(Orders $order): Response
-    {
-        return $this->render('orders/show.html.twig', [
-            'order' => $order,
-        ]);
-    }
+   *
+   * @Route("/{id}",methods={"GET"} ,name="app_orders_show")
+   */
+  public function showAction(Orders $orders)
+  {
+    $deleteForm = $this->createDeleteForm($orders);
 
-    /**
+    return $this->render('orders/show.html.twig', array(
+      'orders' => $orders,
+      'delete_form' => $deleteForm->createView(),
+    ));
+  }
+
+  /**
      * @Route("/{id}/edit", name="app_orders_edit", methods={"GET", "POST"})
      */
-    public function edit(Request $request, Orders $order, OrdersRepository $ordersRepository): Response
-    {
-        $form = $this->createForm(OrdersType::class, $order);
-        $form->handleRequest($request);
+  public function editAction(Request $request, Orders $orders)
+  {
+    $deleteForm = $this->createDeleteForm($orders);
+    $editForm = $this->createForm(OrdersType::class, $orders);
+    $editForm->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $ordersRepository->add($order);
-            return $this->redirectToRoute('app_orders_index', [], Response::HTTP_SEE_OTHER);
-        }
+    if ($editForm->isSubmitted() && $editForm->isValid()) {
+      $this->getDoctrine()->getManager()->flush();
 
-        return $this->renderForm('orders/edit.html.twig', [
-            'order' => $order,
-            'form' => $form,
-        ]);
+      return $this->redirectToRoute('app_orders_edit', array('id' => $orders->getId()));
     }
 
-    /**
-     * @Route("/{id}", name="app_orders_delete", methods={"POST"})
-     */
-    public function delete(Request $request, Orders $order, OrdersRepository $ordersRepository): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$order->getId(), $request->request->get('_token'))) {
-            $ordersRepository->remove($order);
-        }
+    return $this->render('orders/edit.html.twig', array(
+      'orders' => $orders,
+      'edit_form' => $editForm->createView(),
+      'delete_form' => $deleteForm->createView(),
+    ));
+  }
 
-        return $this->redirectToRoute('app_orders_index', [], Response::HTTP_SEE_OTHER);
-    }
+  /**
+   *
+   * @Route("/orders/{id}", name="app_orders_delete")
+   */
+  public function deleteAction($id)
+  {
+    $em = $this->getDoctrine()->getManager();
+    $orders = $em->getRepository('App\Entity\Orders')->find($id);
+    $em->remove($orders);
+    $em->flush();
+    
+    $this->addFlash(
+        'error',
+        'Orders deleted'
+    );
+
+    return $this->redirectToRoute('app_orders_index');
+  }
+
+  /**
+   *
+   * @param Orders $orders The order entity
+   *
+   * @return Form The form
+   */
+  private function createDeleteForm(Orders $orders): Form
+  {
+    return $this->createFormBuilder()
+      ->setAction($this->generateUrl('app_orders_delete', array('id' => $orders->getId())))
+      ->setMethod('DELETE')
+      ->getForm();
+      
+  }
 }
-
-
