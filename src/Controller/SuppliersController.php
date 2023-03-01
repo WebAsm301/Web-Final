319287be7adc987a6c1ba2031045e7e0b5aefc2c
DorisLTN <125235893+DorisLTN@users.noreply.github.com>
2023-03-01 20:45:35 +0700

    Update SuppliersController.php

--- src/Controller/SuppliersController.php
+++ src/Controller/SuppliersController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Suppliers;
 use App\Form\SuppliersType;
 use App\Repository\SuppliersRepository;
+use Symfony\Component\Form\Form;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -33,11 +34,15 @@ class SuppliersController extends AbstractController
     public function new(Request $request, SuppliersRepository $suppliersRepository): Response
     {
         $supplier = new Suppliers();
-        $form = $this->createForm(SuppliersType::class, $supplier);
+        $form = $this->createForm('App\Form\SuppliersType', $supplier);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $suppliersRepository->add($supplier);
+            $suppliersRepository = $this->getDoctrine()->getManager();
+            $suppliersRepository->persist($supplier);
+            $suppliersRepository->flush();
+            
             return $this->redirectToRoute('app_suppliers_index', [], Response::HTTP_SEE_OTHER);
         }
 
@@ -50,41 +55,61 @@ class SuppliersController extends AbstractController
     /**
      * @Route("/{id}", name="app_suppliers_show", methods={"GET"})
      */
-    public function show(Suppliers $supplier): Response
+    public function showAction(Suppliers $suppliers)
     {
-        return $this->render('suppliers/show.html.twig', [
-            'supplier' => $supplier,
-        ]);
-    }
+        $deleteForm = $this->createDeleteForm($suppliers);
+    
+        return $this->render('suppliers/show.html.twig', array(
+          'suppliers' => $suppliers,
+          'delete_form' => $deleteForm->createView(),
+        ));
+      }
 
     /**
      * @Route("/{id}/edit", name="app_suppliers_edit", methods={"GET", "POST"})
      */
-    public function edit(Request $request, Suppliers $supplier, SuppliersRepository $suppliersRepository): Response
+    public function editAction(Request $request, Suppliers $suppliers)
     {
-        $form = $this->createForm(SuppliersType::class, $supplier);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $suppliersRepository->add($supplier);
-            return $this->redirectToRoute('app_suppliers_index', [], Response::HTTP_SEE_OTHER);
+        $deleteForm = $this->createDeleteForm($suppliers);
+        $editForm = $this->createForm(SuppliersType::class, $suppliers);
+        $editForm->handleRequest($request);
+    
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+          $this->getDoctrine()->getManager()->flush();
+    
+          return $this->redirectToRoute('app_suppliers_edit', array('id' => $suppliers->getId()));
         }
-
-        return $this->renderForm('suppliers/edit.html.twig', [
-            'supplier' => $supplier,
-            'form' => $form,
-        ]);
-    }
+    
+        return $this->render('suppliers/edit.html.twig', array(
+          'suppliers' => $suppliers,
+          'edit_form' => $editForm->createView(),
+          'delete_form' => $deleteForm->createView(),
+        ));
+      }
     
     /**
-     * @Route("/{id}", name="app_suppliers_delete", methods={"POST"})
-     */
-    public function delete(Request $request, Suppliers $supplier, SuppliersRepository $suppliersRepository): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$supplier->getId(), $request->request->get('_token'))) {
-            $suppliersRepository->remove($supplier);
-        }
+ * @Route("/suppliers/{id}", name="app_suppliers_delete")
+ */
+public function deleteAction($id)
+{
+    $em = $this->getDoctrine()->getManager();
+    $suppliers = $em->getRepository('App\Entity\Suppliers')->find($id);
+    $em->remove($suppliers);
+    $em->flush();
+    
+    $this->addFlash(
+        'error',
+        'Products deleted'
+    );
+    
+    return $this->redirectToRoute('app_suppliers_index');
+}
 
-        return $this->redirectToRoute('app_suppliers_index', [], Response::HTTP_SEE_OTHER);
+    private function createDeleteForm(Suppliers $suppliers): Form
+    {
+      return $this->createFormBuilder()
+        ->setAction($this->generateUrl('app_suppliers_delete', array('id' => $suppliers->getId())))
+        ->setMethod('DELETE')
+        ->getForm();
     }
 }
