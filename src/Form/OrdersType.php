cabfca58c44c10effe47026eec879d8704f3d20c
minhhieu1603 <125235732+minhhieu1603@users.noreply.github.com>
2023-03-02 22:45:46 +0700

    Add files via upload

---
+++ src/Form/OrdersType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Orders;
+use PHPUnit\TextUI\CliArguments\Builder;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OrdersType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('Date')
+            ->add('Delivery')
+            ->add('Status')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Orders::class,
+        ]);
+    }
+}
